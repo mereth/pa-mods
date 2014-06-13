4bd21ca55effe4873412d0e5080763a474195491
mereth <mereth78@gmail.com>
2014-06-13 23:22:32 +0200

    initiliaze the 24h clock right away

--- mereth.twentyfour/ui/mods/mereth.twentyfour/twentyfour.js
+++ mereth.twentyfour/ui/mods/mereth.twentyfour/twentyfour.js
@@ -1,13 +1,15 @@
 (function() {
   // we can't do anything against model.time auto update...
   model.time24 = ko.observable();
-  setInterval(function () {
+  var updateTime24 = function () {
       var d = new Date();
       var hh = d.getHours(),
           MM = d.getMinutes();
       
       model.time24((hh < 10 ? '0' : '') + hh + (MM < 10 ? ':0' : ':') + MM);
-  }, 5000); // refresh every 5s is good enough
+  };
+  setInterval(updateTime24, 5000); // refresh every 5s is good enough
+  updateTime24();
   
   $('div.clock').attr('data-bind', 'text: time24');
 })();
