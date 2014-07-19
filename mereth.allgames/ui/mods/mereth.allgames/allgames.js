d627c225c29cecdaeb8575d442febdad4909a30d
mereth <mereth78@gmail.com>
2014-07-19 17:25:34 +0200

    colorize modded column

--- mereth.allgames/ui/mods/mereth.allgames/allgames.js
+++ mereth.allgames/ui/mods/mereth.allgames/allgames.js
@@ -1,3 +1,8 @@
 (function() {
   model.moddedGameFilters(['modded_no','modded_yes']);
+  
+  $gametmpl = $('#game-list div.modded'); console.log($gametmpl.attr('data-bind'));
+  kobinding = $gametmpl.attr('data-bind') + ", style: { color: cheats_enabled ? '#f00' : ( mods_summary.length > 0 ? '#f80' : '#0f0' ) }";
+  
+  $gametmpl.attr('data-bind', kobinding);
 })();
