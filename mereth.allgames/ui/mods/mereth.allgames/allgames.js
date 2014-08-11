0c8f7bf258c72a29b5d53ba1b0103078c2760df9
mereth <mereth78@gmail.com>
2014-08-11 20:55:23 +0200

    default moddedGameFilter to 'Any' instead of ['any']

--- mereth.allgames/ui/mods/mereth.allgames/allgames.js
+++ mereth.allgames/ui/mods/mereth.allgames/allgames.js
@@ -1,7 +1,7 @@
 (function() {
-  model.moddedGameFilter(['any']);
+  model.moddedGameFilter('Any');
   
-  $gametmpl = $('#game-list div.modded'); console.log($gametmpl.attr('data-bind'));
+  $gametmpl = $('#game-list div.modded');
   kobinding = $gametmpl.attr('data-bind') + ", style: { color: cheats_enabled ? '#f00' : ( mods_summary.length > 0 ? '#f80' : '#0f0' ) }";
   
   $gametmpl.attr('data-bind', kobinding);
